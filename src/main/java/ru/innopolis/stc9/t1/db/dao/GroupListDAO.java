3385bf601876343f86dcb67f2f179b56303dd479
mtelkov <m.telkov@yandex.ru>
2018-06-16 00:08:20 +0300

    At now we can add user to group.

--- src/main/java/ru/innopolis/stc9/t1/db/dao/GroupListDAO.java
+++ src/main/java/ru/innopolis/stc9/t1/db/dao/GroupListDAO.java
@@ -7,4 +7,5 @@ import java.util.ArrayList;
 public interface GroupListDAO {
     boolean deleteUserFromGroup(int group_id, int user_id);
     ArrayList<User> getGroupList(int group_id);
+    boolean addUserToGroup(int group_id, int user_id);
 }
